0fdba84b2db1efdea7710bb25b5e24d61e28738b
KaushikWadhavana <wadhvanakaushik@gmail.com>
2025-06-03 13:06:12 +0530

    2-6 add data in html page

--- back/app.js
+++ back/app.js
@@ -1,23 +1,36 @@
+const { MongoClient } = require('mongodb');
 const lib = require("express")
+const cors = require("cors");
 
 const server = lib()
+server.use(cors());
 
 //route or API one is post and another is get
-server.post("/users", (req,res)=>{
-    console.log("request received")
-    res.send("user created")
+server.post("/users", (req, res) => {
+  console.log("request received")
+  res.send("user created")
 })
-server.post("/player", (req,res)=>{
-    console.log("request received")
-    res.send("Player is Created")
+server.get("/users", (req, res) => {
+  const connection = new MongoClient("mongodb://kaushik:kaushik123@localhost:27017/IMS")
+  connection.connect().
+  then(()=>connection.db()).
+  then((db)=>db.collection('users')).
+  then((collection)=>collection.find().toArray()).
+  then((result)=>console.log(result)).
+  // then((result)=>res.json(result)). for peint in html page
+  catch((err)=>console.log(err))
 })
-server.post("/Team", (req,res)=>{
-    console.log("request received")
-    res.send("Team is Created.")
+server.post("/player", (req, res) => {
+  console.log("request received")
+  res.send("Player is Created")
+})
+server.post("/Team", (req, res) => {
+  console.log("request received")
+  res.send("Team is Created.")
 })
 
-server.listen(8000, ()=>{
-    console.log("server is ready and listening order port 8000")
-    
+server.listen(8000, () => {
+  console.log("server is ready and listening order port 8000")
+
 
 })
